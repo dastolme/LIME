a179792a82c62f9d05494707dfea378d182c3d7b
dastolme <melba.dastolfo@gmail.com>
2024-12-23 18:10:11 +0100

    Remove the exponential contribution of the fit

--- calibration.py
+++ calibration.py
@@ -13,33 +13,26 @@ class Calibration:
         analysis_calib_data = AnalysisManager(self.calibration_df)
         analysis_calib_data.apply_quality_cuts().apply_fiducial_cuts(400,1900,400,1900).apply_slimness_cut(0.8)
 
-        counts, bin_edges = np.histogram(analysis_calib_data.dataframe_cut['sc_integral'], bins = 600, range = (0,iron_peak_window[1]))
+        counts, bin_edges = np.histogram(analysis_calib_data.dataframe_cut['sc_integral'], bins = 200, range = (0,iron_peak_window[1]))
         bin_widths = np.diff(bin_edges)
         x = bin_edges[:-1] + (bin_widths / 2)
         lower_index_iron_window = np.abs(x - iron_peak_window[0]).argmin()
         upper_index_iron_window = np.abs(x - iron_peak_window[1]).argmin()
 
-        background = ExponentialModel(prefix='exp_')
         iron_peak = GaussianModel(prefix='g1_')
-        model = background + iron_peak
+        model = iron_peak
 
-        exp_amplitude = np.max(counts[:lower_index_iron_window])
-        exp_peak_index = np.where(counts == exp_amplitude)[0][0]
-        half_life_index = np.abs(counts[:lower_index_iron_window] - exp_amplitude/2).argmin()
-        exp_decay = np.log(2) * x[half_life_index]
         g1_amplitude = np.sum([bin_widths[i] * counts[i] for i in np.arange(lower_index_iron_window,upper_index_iron_window)])
         g1_height = np.max(counts[lower_index_iron_window:upper_index_iron_window])
         g1_center = x[np.where(counts == g1_height)]
         g1_sigma = g1_amplitude/ (g1_height * np.sqrt(2 * np.pi))
+        params = model.make_params(g1_amplitude=g1_amplitude, g1_center=g1_center, g1_sigma=g1_sigma)
+
         
-        params = model.make_params(g1_amplitude=g1_amplitude, g1_center=g1_center, g1_sigma=g1_sigma, 
-                                   exp_amplitude=exp_amplitude, exp_decay=exp_decay)
-        
-        result = model.fit(counts[exp_peak_index:], params, x=x[exp_peak_index:])
+        result = model.fit(counts, params, x=x)
         comps = result.eval_components()
-        plt.plot(x[exp_peak_index:], counts[exp_peak_index:], 'o', markersize = '2')
-        plt.plot(x[exp_peak_index:], comps['g1_'], '--', label='Gaussian component')
-        plt.plot(x[exp_peak_index:], comps['exp_'], '--', label='Exp component')
+        plt.plot(x, counts, 'o', markersize = '2')
+        plt.plot(x, comps['g1_'], '--', label='Gaussian component')
         plt.show()
 
         return result
@@ -56,7 +49,7 @@ def main():
     calibration_df = pd.concat(calibration_list)
 
     calibration = Calibration(data_df, calibration_df)
-    iron_peak_window = [2_000,8_000]
+    iron_peak_window = [2_000,6_000]
     fit_result = Calibration.fit_iron_peak(calibration, iron_peak_window)
     print(fit_result.fit_report())
 
